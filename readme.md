255680cc42cbcb725c6e1c1493b1114301532ac3
u89man <u89man@gmail.com>
2020-07-25 00:19:49 +0500

    Инициализация проекта

---
+++ readme.md
@@ -0,0 +1,26 @@
+Database 0.1.0
+
+```php
+use U89Man\Database\DB;
+
+$host = 'localhost';
+$dbname = 'database';
+$user = 'user';
+$passwd = 'password';
+
+$dsn = 'mysql:dbname='.$dbname.';host='.$host;
+
+$options = [
+    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+    PDO::ATTR_CASE => PDO::CASE_NATURAL,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+];
+
+$pdo = new PDO($dsn, $user, $passwd, $options);
+
+$db = new DB($pdo);
+
+```
+
+
---
+++ src/DB.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace U89Man\Database;
+
+use PDO;
+use PDOException;
+use PDOStatement;
+
+class DB
+{
+    /**
+     * @var PDO
+     */
+    protected $pdo;
+
+
+    /**
+     * @param PDO $pdo
+     */
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * @param string $sql
+     * @param array|null $params
+     *
+     * @return PDOStatement|null
+     */
+    public function query($sql, array $params = null)
+    {
+        try {
+            $sql = trim($sql, ' ;');
+
+            $stmt = $this->pdo->prepare($sql);
+
+            if ($stmt->execute($params)) {
+                return $stmt;
+            }
+        }
+        catch (PDOException $e) {
+            $this->report($e);
+        }
+
+        return null;
+    }
+
+    /**
+     * @param PDOException $e
+     *
+     * @return void
+     */
+    public function report(PDOException $e)
+    {
+        throw $e;
+    }
+
+    /**
+     * @param string $table
+     * @param array $data
+     *
+     * @return int
+     */
+    public function insert($table, array $data)
+    {
+        $params = [];
+        foreach ($data as $key => $value) {
+            $params[':'.$key] = $value;
+        }
+
+        $columns = implode(', ', array_keys($data));
+        $values = implode(', ', array_keys($params));
+
+        $sql = 'INSERT INTO '.$table.' ('.$columns.') VALUES ('.$values.')';
+
+        if ($this->query($sql, $params)) {
+            return (int) $this->pdo->lastInsertId();
+        }
+
+        return -1;
+    }
+
+    /**
+     * @param string $table
+     * @param string[]|string $columns
+     * @param string|null $where
+     * @param string|null $order
+     * @param array|null $params
+     *
+     * @return array
+     */
+    public function select($table, $columns = '*', $where = null, $order = null, $params = null)
+    {
+        $columns = is_string($columns) ? $columns : implode(', ', $columns);
+
+        $sql = 'SELECT '.$columns.' FROM '.$table;
+
+        if ($where)
+            $sql .= ' WHERE '.$where;
+
+        if ($order)
+            $sql .= ' ORDER BY '.$order;
+
+        if ($stmt = $this->query($sql, $params)) {
+            return $stmt->fetchAll();
+        }
+
+        return [];
+    }
+
+    /**
+     * @param string $table
+     * @param array $data
+     * @param string|null $where
+     * @param array|null $params
+     *
+     * @return int
+     */
+    public function update($table, array $data, $where = null, array $params = null)
+    {
+        $setData = array_map(function ($k, $v) { return $k.' = "'.$v.'"'; }, array_keys($data), array_values($data));
+
+        $sql = 'UPDATE '.$table.' SET '.implode(', ', $setData);
+
+        if ($where)
+            $sql .= ' WHERE '.$where;
+
+        if ($stmt = $this->query($sql, $params)) {
+            return $stmt->rowCount();
+        }
+
+        return -1;
+    }
+
+    /**
+     * @param string $table
+     * @param string|null $where
+     * @param array|null $params
+     *
+     * @return int
+     */
+    public function delete($table, $where = null, array $params = null)
+    {
+        $sql = 'DELETE FROM '.$table;
+
+        if ($where)
+            $sql .= ' WHERE '.$where;
+
+        if ($stmt = $this->query($sql, $params)) {
+            return $stmt->rowCount();
+        }
+
+        return -1;
+    }
+}
+
